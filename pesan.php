41b237539ded844220c3121b7fcbddd8b16c4793
Muhammad Farhan Amien <farhanexcellentz17@gmail.com>
2024-11-12 18:36:26 +0700

    Menambahkan kode untuk mengirim email ke penerima yang ditentukan

--- pesan.php
+++ pesan.php
@@ -7,6 +7,12 @@ $pesan = $_POST['pesan'] ?? '';
 
 if (!empty($email) && !empty($pesan)){
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $penerima = "farhanexcellentz17@gmail.com";
+        $subjek = "Pesan dari : $nama <$email>";
+        $body = "Nama: $nama\n\nEmail: $email\n\nTelepon: $telepon\n\nLokasi: $lokasi\n\nPesan:\n$pesan\n\nSalam,\n$nama";
+        $header = "From: $email";
+
         
+
     }
 }
